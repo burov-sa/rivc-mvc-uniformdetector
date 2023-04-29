eef11808f658f07ac8ae702d8df1d9a928971aa4
Буров Сергей <s.burov@rivc-pulkovo.ru>
2023-04-29 06:06:40 +0300

    second

--- train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Загрузка модели
-model = YOLO("yolov8n.yaml")  # построение новой модели
-
-# Использование модели
-results = model.train(data="config.yaml", epochs=50)  # Обучение модели на размеченных данных
